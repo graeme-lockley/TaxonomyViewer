622548146c3fa63bcb8e751505fa959d4f37f07e
Graeme Lockley <graemel@no9.co.za>
2018-02-11 14:59:44 +0200

    refactor: create a Main.elm and remove the old counter

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Elm = require('./src/elm/counter.elm');
+var Elm = require('./src/elm/Main.elm');
 var mountNode = document.getElementById('elm-app');
 
 // The third value on embed are the initial values for incoming ports into Elm
