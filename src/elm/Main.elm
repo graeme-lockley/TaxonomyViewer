f83047786c86d71f3fa46ba2b46563c29e9a8084
Graeme Lockley <graemel@no9.co.za>
2018-02-11 20:12:26 +0200

    feature: get the basics in place to start rendering the taxonomies

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -2,7 +2,9 @@ import Html exposing (..)
 import Html.Events exposing (onClick)
 
 
-type alias Model = Int
+type alias Model = { counter : Int }
+
+
 type Action = NoOp | Inc | Dec
 
 
@@ -10,26 +12,30 @@ update : Action -> Model -> ( Model, Cmd Action)
 update action model =
   case action of
     NoOp -> (model, Cmd.none)
-    Inc -> (model + 1, Cmd.none)
-    Dec -> (model - 1, Cmd.none)
+    Inc -> ({ model | counter = model.counter + 1}, Cmd.none)
+    Dec -> ({ model | counter = model.counter - 1}, Cmd.none)
 
 
 view : Model -> Html Action
 view model =
-  div [] [
-    div [] [text "Counter"],
-    div [] [text ("From model :: " ++ (toString model))],
-    div [] [
-      button [onClick Dec] [text "--"],
-      span [] [text (toString model)],
-      button [onClick Inc] [text "++"]
-    ]
-  ]
+    div []
+        [ table []
+            [
+            ]
+        , div [] [text "Counter"]
+        , div [] [text ("From model :: " ++ (toString model))]
+        , div []
+            [ button [onClick Dec] [text "--"]
+            , span [] [text (toString model)]
+            , button [onClick Inc] [text "++"]
+            ]
+        ]
+
 
 
 init : ( Model, Cmd Action )
 init =
-  (0, Cmd.none)
+  ({counter = 0}, Cmd.none)
 
 
 subscriptions : Model -> Sub Action
